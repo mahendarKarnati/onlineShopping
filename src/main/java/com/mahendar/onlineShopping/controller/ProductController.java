972395292d65211d396df5ecc8c07c68b2f178e3
mahendar karnati <mahendar.karnati@gmail.com>
2025-08-06 15:42:43 +0530

    Update ProductController.java

--- src/main/java/com/mahendar/onlineShopping/controller/ProductController.java
+++ src/main/java/com/mahendar/onlineShopping/controller/ProductController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import com.mahendar.onlineShopping.model.Booking;
+import org.springframework.web.filter.CorsFilter;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
