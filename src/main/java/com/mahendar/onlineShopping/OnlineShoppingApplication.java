c1896ddde1b550baae461dee14f32e68f18b213d
unknown <mahendar.karnati@gmail.com>
2025-08-08 10:06:31 +0530

    updated application.properties and OnlineShoppingAplication.java

--- src/main/java/com/mahendar/onlineShopping/OnlineShoppingApplication.java
+++ src/main/java/com/mahendar/onlineShopping/OnlineShoppingApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import jakarta.annotation.PostConstruct;
+
 //@SpringBootApplication
 //public class OnlineShoppingApplication {
 //	public static void main(String[] args) {
@@ -17,6 +19,15 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class OnlineShoppingApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineShoppingApplication.class, args);
+        
     }
+    @PostConstruct
+    public void checkEnvVars() {
+        System.out.println("=== ENV VAR CHECK ===");
+        System.out.println("DB_NAME: " + System.getenv("DB_NAME"));
+        System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+        System.out.println("DB_PASSWORD: " + (System.getenv("DB_PASSWORD") != null ? "SET" : "NOT SET"));
+    }
+
 }
 
