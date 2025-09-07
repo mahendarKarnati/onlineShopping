877f63de59e046dfe5563348fc9470f1b3228020
mahendar karnati <mahendar.karnati@gmail.com>
2025-09-07 20:27:06 +0530

    Update CloudinaryConfig.java

--- backend/src/main/java/com/mahendar/onlineShopping/config/CloudinaryConfig.java
+++ backend/src/main/java/com/mahendar/onlineShopping/config/CloudinaryConfig.java
@@ -14,14 +14,12 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-//        config.put("cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"));
-//        config.put("api_key", System.getenv("CLOUDINARY_API_KEY"));
-//        config.put("api_secret", System.getenv("CLOUDINARY_API_SECRET"));
+    config.put("cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"));
+   config.put("api_key", System.getenv("CLOUDINARY_API_KEY"));
+   config.put("api_secret", System.getenv("CLOUDINARY_API_SECRET"));
         
         
-        config.put("cloud_name", "dkvvq7sdp");
-        config.put("api_key", "576736224996614");
-        config.put("api_secret", "1xmzftZs_2JChiD5CDU-DTU6rSA");
+        
         return new Cloudinary(config);
     }
 }
